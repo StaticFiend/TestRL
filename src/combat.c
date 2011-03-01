deba6612f956791d715ccd18540463b77361223b
Nick Franklin <static_fiend@circa1984.com>
2011-03-01 14:35:18 -0800

    Since moving to 2D Arrays for the map character, I fixed the lava check for damage.

--- src/combat.c
+++ src/combat.c
@@ -1,10 +1,14 @@
 #include "../include/trl-main.h"
 
-void check_tile(object_t *object) {
-	//We use this to check if a tile will hurt us.
-	//FIXME: Current system really doesn't allow this easily, change to 2D arra
-	if (TCOD_console_get_char(NULL, object->x, object->y) == LAVA)
-		object->currentHP = object->currentHP - (object->maxHP * 0.10);
+void check_tile(object_t *object, char map[][MAP_WIDTH]) {
+	//We use this to check if a tile will hurt us, or inflict some status effect.
+	//Also make sure to subtract 5 from the object's y value, as we need to translate this to
+	//how the map system works.
+	if (map[object->y - 5][object->x] == LAVA)
+		//10% health taken per step on lava.
+		object->currentHP = ceil(object->maxHP * 0.10);
+//	else if (map[object->y - 5][object->x] == BLOOD)
+//	TODO: Think about something for blood, maybe other tiles too.
 }
 
 void check_attack(object_t *player, object_t *monster, int damage, int attacker) {
